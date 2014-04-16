e13430526991c940416a7751217841c16b82a2ff
David Parrish <daveparrish@gmail.com>
2014-04-16 05:04:53 -0400

    Initial setup of VM
    - Bash as provisioning script
    - Arch Linux as server platform

---
+++ bootstrap.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Update pacman
+echo "Updating Arch"
+pacman -Syu --noconfirm
+
+# Install php
+echo "Trying to install PHP and MySQL"
+
+echo "Trying to start MySQL"
+
+echo "Trying to create MySQL database"
